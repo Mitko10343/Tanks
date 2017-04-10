4c58f54ff5fffc7c3aecde43e2f5bbcb1c9b017f
Mitko10343 <c15334276@mydit.ie>
2017-04-10 17:32:43 +0100

    Movement script complete

--- Assets/BasicMovement.cs
+++ Assets/BasicMovement.cs
@@ -4,8 +4,89 @@ using UnityEngine;
 
 public class BasicMovement : MonoBehaviour {
 
+    public int PlayerNum = 1;
+    public float Speed = 12f;
+    public float turnSpeed = 180f;
 
-    public float thrust = 0;
+    private string MovementAxisName;
+    private string TurnAxisName;
+    private Rigidbody tank;
+    private float MovementInputValue;
+    private float TurnInputValue;
+
+
+    private void Awake()
+    {
+        tank = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+        tank.isKinematic = false;
+        MovementInputValue = 0f;
+        TurnInputValue = 0f;
+    }
+
+    private void OnDisable()
+    {
+        tank.isKinematic = true;
+    }
+
+    private void Start()
+    {
+        MovementAxisName = "Vertical" + PlayerNum;
+        TurnAxisName = "Horizontal" + PlayerNum;
+    }
+
+    private void Update()
+    {
+        //Store the player input
+        MovementInputValue = Input.GetAxis(MovementAxisName);
+        TurnInputValue = Input.GetAxis(TurnAxisName);
+    }
+
+    private void FixedUpdate()
+    {
+        //Move and turn the tank
+        Move();
+        Turn();
+    }
+
+
+    private void Move()
+    {
+        // Adjust the position of the tank based on the player's input
+        Vector3 movement = transform.forward * MovementInputValue * Speed * Time.deltaTime;
+
+        tank.MovePosition(tank.position + movement);
+    }
+
+    private void Turn()
+    {
+        //Adjust the rotation of the tank based on the player's inpout
+        float turn = TurnInputValue * turnSpeed * Time.deltaTime;
+
+        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+
+        tank.MoveRotation(tank.rotation * turnRotation);
+    }
+    /*
+     private void EngineAudio()
+     {
+        This function will be for adding sound to the tank
+
+        if(Tank is moving)
+        {
+            play the sound for when tank is moving
+        }
+        else if( tank is idle )
+        {
+            Play sound for tank stationary
+        }
+     }
+    */
+
+    /*public float thrust = 0;
     public Rigidbody rb;
 
     public float torque;
@@ -79,7 +160,7 @@ public class BasicMovement : MonoBehaviour {
             //turn = Input.GetAxis("Horizontal");
             //rb.AddRelativeTorque(0, torque, 0);
         }
-        */
-    }
+        
+    }*/
 }
 
