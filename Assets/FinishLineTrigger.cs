e7130f0954fbe9bd3d2ac26fa7f1649d421ba0f9
Mitko10343 <c15334276@mydit.ie>
2017-04-20 17:18:39 +0100

    Finish Line Added

---
+++ Assets/FinishLineTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishLineTrigger : MonoBehaviour {
+
+	
+    void OnTriggerEnter(Collider end)
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+}
