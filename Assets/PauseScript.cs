09b960813359e99d6487ded03d6f85884085787c
Mitko10343 <c15334276@mydit.ie>
2017-04-25 00:20:25 +0100

    Making a paused menu

---
+++ Assets/PauseScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour {
+
+    public GameObject[] guiObjects;
+
+    public bool pause = false;
+
+    public void Start()
+    {
+        Time.timeScale = 1;
+        guiObjects = GameObject.FindGameObjectsWithTag("Paused");
+    }
+    public void update()
+    {
+        if (Input.GetKeyDown("p") && pause == false)
+        {
+            if (Time.timeScale == 0)
+            {
+                foreach(GameObject g in guiObjects)
+                {
+                    g.SetActive(true);
+                }
+            }
+            else if(Time.timeScale == 1)
+            {
+                foreach (GameObject g in guiObjects)
+                {
+                    g.SetActive(false);
+                }
+
+            }
+        }
+    }
+}
